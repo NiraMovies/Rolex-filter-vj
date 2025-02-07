4c28073512c638b7d809dfc52ca00cd37dbe5196
NiraMovies <naveenbalaji2nb03@gmail.com>
2025-02-07 14:02:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -86,7 +86,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+-TasIeJTBa5iOGRl')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Niramovies_LinkZz')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'nira_request_group') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Naveenvnb')
 
 # True Or False
